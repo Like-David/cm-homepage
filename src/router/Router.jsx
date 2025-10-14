69efe7a522ad1e675fb116ee75aee66657a7fed5
leejimin <leejimin00@naver.com>
2025-10-14 16:40:40 +0900

    수정사항

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import MainLayout from '@/layouts/MainLayout';
 import Index from '@/pages/Index';
 import SolutionPage from '@/pages/SolutionPage';
+//  확인
 import { ClientPage } from '../pages/ClientPage';
 
 function Router() {

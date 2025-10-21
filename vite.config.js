05535929f1e13ccd1b4c3b744db6056c641482e8
leejimin <leejimin00@naver.com>
2025-10-21 17:13:46 +0900

    fix: vite 설정

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -14,6 +13,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
       '/eform-demo': {
         target: 'http://localhost:8080',
         changeOrigin: true,

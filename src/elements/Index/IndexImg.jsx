ca2cbdc7f54dbce10fa579388048d51bc7ff9ca1
leejimin <leejimin00@naver.com>
2026-03-27 14:02:28 +0900

    style : 사업영역 분류체계 수정 및 솔루션 테스트 추가

--- src/elements/Index/IndexImg.jsx
+++ src/elements/Index/IndexImg.jsx
@@ -8,7 +8,7 @@ function IndexImg() {
     const { t } = useTranslation();
     return (
         <div className="homepage-container">
-            <Image src={image} fluid className="homepage-main-image" style={{ filter: 'brightness(70%)' }} />
+            <img src={image} alt="Hero" className="homepage-main-image" style={{ filter: 'brightness(70%)' }} />
             <div className="homepage-text-overlay">
                 <p className="homepage-main-text">{t('info.index_overlay_1')}</p>
                 <p className="homepage-main-text">{t('info.index_overlay_2')}</p>
